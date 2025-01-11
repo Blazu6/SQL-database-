5e21a745e7a3d5665061cce05287358c9aa4c2ee
Blazu6 <114349938+Blazu6@users.noreply.github.com>
2025-01-11 13:37:16 +0100

    Create DML.sql

---
+++ DML.sql
@@ -0,0 +1,69 @@
+-- Wstawianie danych do tabeli KATEGORIE
+INSERT INTO kategorie (id_kategori, nazwa_kategori) VALUES (1, 'Elektronika');
+INSERT INTO kategorie (id_kategori, nazwa_kategori) VALUES (2, 'AGD');
+INSERT INTO kategorie (id_kategori, nazwa_kategori) VALUES (3, 'RTV');
+INSERT INTO kategorie (id_kategori, nazwa_kategori) VALUES (4, 'Komputery');
+INSERT INTO kategorie (id_kategori, nazwa_kategori) VALUES (5, 'Telefony');
+
+-- Wstawianie danych do tabeli KLIENCI
+INSERT INTO klienci (id_klienta, imie, nazwisko, adres, email, telefon) 
+VALUES (1, 'Jan', 'Kowalski', 'ul. Warszawska 1, 00-001 Warszawa', 'jan.kowalski@email.com', '123456789');
+
+INSERT INTO klienci (id_klienta, imie, nazwisko, adres, email, telefon) 
+VALUES (2, 'Anna', 'Nowak', 'ul. Krakowska 2, 00-002 Kraków', 'anna.nowak@email.com', '987654321');
+
+INSERT INTO klienci (id_klienta, imie, nazwisko, adres, email, telefon) 
+VALUES (3, 'Piotr', 'Wiśniewski', 'ul. Gdańska 3, 00-003 Gdańsk', 'piotr.wisniewski@email.com', '564738291');
+
+INSERT INTO klienci (id_klienta, imie, nazwisko, adres, email, telefon) 
+VALUES (4, 'Katarzyna', 'Zielińska', 'ul. Poznańska 4, 00-004 Poznań', 'katarzyna.zielinska@email.com', '412536879');
+
+INSERT INTO klienci (id_klienta, imie, nazwisko, adres, email, telefon) 
+VALUES (5, 'Marek', 'Jankowski', 'ul. Wrocławska 5, 00-005 Wrocław', 'marek.jankowski@email.com', '987653215');
+
+INSERT INTO klienci (id_klienta, imie, nazwisko, adres, email, telefon) 
+VALUES (6, 'Monika', 'Dąbrowska', 'ul. Lublin 6, 00-006 Lublin', 'monika.dabrowska@email.com', '623547890');
+
+INSERT INTO klienci (id_klienta, imie, nazwisko, adres, email, telefon) 
+VALUES (7, 'Łukasz', 'Szymański', 'ul. Katowicka 7, 00-007 Katowice', 'lukasz.szymanski@email.com', '237489056');
+
+INSERT INTO klienci (id_klienta, imie, nazwisko, adres, email, telefon) 
+VALUES (8, 'Julia', 'Kaczmarek', 'ul. Zielona 8, 00-008 Zielona Góra', 'julia.kaczmarek@email.com', '478392145');
+
+INSERT INTO klienci (id_klienta, imie, nazwisko, adres, email, telefon) 
+VALUES (9, 'Adam', 'Woźniak', 'ul. Łódzka 9, 00-009 Łódź', 'adam.wozniak@email.com', '346579081');
+
+INSERT INTO klienci (id_klienta, imie, nazwisko, adres, email, telefon) 
+VALUES (10, 'Barbara', 'Wiśniewska', 'ul. Bydgoska 10, 00-010 Bydgoszcz', 'barbara.wisniewska@email.com', '123987654');
+
+INSERT INTO klienci (id_klienta, imie, nazwisko, adres, email, telefon) 
+VALUES (11, 'Grzegorz', 'Lewandowski', 'ul. Olsztyńska 11, 00-011 Olsztyn', 'grzegorz.lewandowski@email.com', '563789432');
+
+INSERT INTO klienci (id_klienta, imie, nazwisko, adres, email, telefon) 
+VALUES (12, 'Ewa', 'Chmielewska', 'ul. Śląska 12, 00-012 Rzeszów', 'ewa.chmielewska@email.com', '478612356');
+
+INSERT INTO klienci (id_klienta, imie, nazwisko, adres, email, telefon) 
+VALUES (13, 'Michał', 'Majewski', 'ul. Gorzysława 13, 00-013 Gorzów Wielkopolski', 'michal.majewski@email.com', '246803659');
+
+INSERT INTO klienci (id_klienta, imie, nazwisko, adres, email, telefon) 
+VALUES (14, 'Karolina', 'Sienkiewicz', 'ul. Wileńska 14, 00-014 Kielce', 'karolina.sienkiewicz@email.com', '532794860');
+
+INSERT INTO klienci (id_klienta, imie, nazwisko, adres, email, telefon) 
+VALUES (15, 'Piotr', 'Mazurek', 'ul. Mazurska 15, 00-015 Gdynia', 'piotr.mazurek@email.com', '312845790');
+
+-- Wstawianie danych do tabeli KOOPERANCI
+INSERT INTO kooperanci (id_kooperanta, typ, nazwa, adres, telefon) 
+VALUES (1, 'Firma dowozowa', 'TechnoSped', 'ul. Logistyczna 1, 00-101 Warszawa', '123654789');
+
+INSERT INTO kooperanci (id_kooperanta, typ, nazwa, adres, telefon) 
+VALUES (2, 'Firma transportowa', 'GlobalTech', 'ul. Przemysłowa 2, 00-102 Kraków', '987654321');
+
+INSERT INTO kooperanci (id_kooperanta, typ, nazwa, adres, telefon) 
+VALUES (3, 'Firma spedycyjna', 'PolTrans', 'ul. Transportowa 3, 00-103 Wrocław', '564738291');
+
+INSERT INTO kooperanci (id_kooperanta, typ, nazwa, adres, telefon) 
+VALUES (4, 'Firma kurierska', 'SpeedyDelivery', 'ul. Ekspresowa 4, 00-104 Poznań', '412536879');
+
+INSERT INTO kooperanci (id_kooperanta, typ, nazwa, adres, telefon) 
+VALUES (5, 'Firma logistyczna', 'LogisticsPro', 'ul. Nowa 5, 00-105 Gdańsk', '987653215');
+
